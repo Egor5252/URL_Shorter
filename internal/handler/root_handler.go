0a5e0a192ccdd97ee2e6214ad014c6d4e6d1ca86
Egor <egor.garbar2014@gmail.com>
2025-07-29 10:46:37 +0300

    NORoot

--- internal/handler/root_handler.go
+++ internal/handler/root_handler.go
@@ -9,3 +9,7 @@ import (
 func RootHandler(c *gin.Context) {
 	c.HTML(http.StatusOK, "index.html", nil)
 }
+
+func NoPage(c *gin.Context) {
+	c.HTML(http.StatusOK, "index.html", nil)
+}
--- internal/router/router.go
+++ internal/router/router.go
@@ -8,10 +8,11 @@ import (
 )
 
 func InitRouters(r *gin.Engine) {
-	// r.Static("assets", "web-app/dist/assets")
-	// r.StaticFile("vite.svg", "web-app/dist/vite.svg")
-	// r.LoadHTMLFiles("web-app/dist/index.html")
-	// r.GET("/", handler.RootHandler)
+	r.Static("assets", "web-app/dist/assets")
+	r.StaticFile("vite.svg", "web-app/dist/vite.svg")
+	r.LoadHTMLFiles("web-app/dist/index.html")
+	r.GET("/", handler.RootHandler)
+	r.NoRoute(handler.NoPage)
 
 	r.POST("/register", handler.Register)
 	r.POST("/login", handler.Login)
